3fe3186cf35fc062a1bef0fe86499d17db28f6d4
ogabek <norpulatovogabek42@gmail.com>
2022-06-21 16:06:06 +0500

    notoddnumbers

--- bin/odd.dart
+++ bin/odd.dart
@@ -1,4 +1,24 @@
 void main() {
-  int x = 43;
-  print(x % 2);
+  int n = 2168;
+  int x1, x2, x3, x4;
+
+  x1 = n % 10;
+  n = n ~/ 10;
+
+  x2 = n % 10;
+  n ~/= 10;
+
+  x3 = n % 10;
+  n ~/= 10;
+
+  x4 = n % 10;
+
+  int s = 0;
+
+  s = s + x1 % 2;
+  s = s + x2 % 2;
+  s = s + x3 % 2;
+  s = s + x4 % 2;
+
+  print(s);
 }
